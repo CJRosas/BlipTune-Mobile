0c6c97215c5af003fea5be742ceacc61656a192e
CJRosas <cjrosas.dev@gmail.com>
2022-05-26 12:58:20 -0500

    Added slider UI, nonfunctional yet

--- Assets/MovementScript.cs
+++ Assets/MovementScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.Experimental.XR;
+using UnityEngine.EventSystems;
 
 public class MovementScript : MonoBehaviour
 {
@@ -12,7 +13,6 @@ public class MovementScript : MonoBehaviour
     private GameObject placedObject;
     private Vector2 touchPosition = default;
     private ARRaycastManager arRaycastManager;
-    private bool onTouchHold = false;
     private static List<ARRaycastHit> hits = new List<ARRaycastHit>();
     private int activeBlips = 1;
 
@@ -25,22 +25,28 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If touch detected
         if(Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
 
             touchPosition = touch.position;
 
-            if(touch.phase == TouchPhase.Began)
+            // If first touch detected and the pointer isnt over the UI (Prevent tapping through UI)
+            if(touch.phase == TouchPhase.Began && !EventSystem.current.IsPointerOverGameObject(touch.fingerId))
             {
+                // Create ray from camera into world space
                 Ray ray = arCamera.ScreenPointToRay(touch.position);
                 RaycastHit hitObject;
+
+                // If ray hits a blip, delete it
                 if(Physics.Raycast(ray, out hitObject))
                 {
-
                     Destroy(hitObject.transform.gameObject);
                     activeBlips -= 1;
                 }
+
+                // Otherwise, create a new blip 0.5m away from camera with appropriate frequency
                 else
                 {        
                     activeBlips += 1;
@@ -48,22 +54,13 @@ public class MovementScript : MonoBehaviour
                     Instantiate(placedPrefab, ray.GetPoint(0.5f), Quaternion.identity);
                 }
             }
+
+            // Code for exponentially decreasing vol of all blips to avoid aliasing (1/N)
             GameObject[] spheres = GameObject.FindGameObjectsWithTag("Untagged");
             for(int i = 0; i < spheres.Length; i++)
             {
                 spheres[i].GetComponent<AudioSource>().volume = (1 / activeBlips);
             }
         }
-        // if(arRaycastManager.Raycast(touchPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.All))
-        // {
-        //     Pose hitPose = hits[0].pose;
-
-
-
-        //     if(onTouchHold)
-        //     {
-        //         placedObject.transform.position = hitPose.position;
-        //     }
-        // }
     }
 }
